a1d7cbfb4a51fb0de7a1961d9425a841114a0071
Joselymug <155993774+Joselymug@users.noreply.github.com>
2025-07-01 12:38:33 -0500

    agregar clases modelo

---
+++ ConsoleApp1/Bodega.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Bodega
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public List<Producto> Productos { get; set; }
+        public Bodega(string nombre, string direccion, string telefono)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Productos = new List<Producto>();
+        }
+        public void AgregarProducto(Producto producto)
+        {
+            Productos.Add(producto);
+        }
+        public void MostrarProductos()
+        {
+            foreach (var producto in Productos)
+            {
+                Console.WriteLine($"Nombre: {producto.Nombre}, Precio: {producto.Precio}");
+            }
+        }
+    }
+}
